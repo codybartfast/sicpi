a7342bbca7825e6a5bfde33b7dca68104450dba7
John Donnellan <john@codybartfast.com>
2023-07-29 10:48:38 +0000

    Wip: clear errno before call (integer happy path working)

--- src/parser.c
+++ src/parser.c
@@ -2,27 +2,29 @@
 
 #include <errno.h>
 #include <stdlib.h>
-// #include <string.h>
 
-#define MAX_DIGITS 18
 static object number(token tkn)
 {
 	long long conv_int;
-	long double conv_flt;
 	char *end = NULL;
 	char *s = tkn_text(tkn);
+	errno = 0;
 	conv_int = strtoll(s, &end, 0);
+	// check string number doesn't overflow long long and
+	// long long doesn't overflow whatever Integer is defined as.
 	if (end && !*end && errno != ERANGE && integer_min <= conv_int &&
 	    conv_int <= integer_max) {
 		return from_integer(conv_int, NO_META_DATA);
 	}
-	end = NULL;
-	conv_flt = strtold(s, &end);
-	if (end && !*end && errno != ERANGE && floating_min <= conv_flt &&
-	    conv_flt <= floating_max) {
-		return from_integer(-1, NO_META_DATA);
-		// return from_floating(conv_flt, NO_META_DATA);
-	}
+	// long double conv_flt;
+	// end = NULL;
+	// // errno = 0
+	// conv_flt = strtold(s, &end);
+	// if (end && !*end && errno != ERANGE && floating_min <= conv_flt &&
+	//     conv_flt <= floating_max) {
+	// 	return from_integer(-1, NO_META_DATA);
+	// 	// return from_floating(conv_flt, NO_META_DATA);
+	// }
 	return from_integer(-1, NO_META_DATA);
 }
 
