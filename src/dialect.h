ede0950d23c8e3af9d0f790800876f6afa058e99
John Donnellan <john@codybartfast.com>
2023-09-27 09:33:16 +0000

    Mod: make_prim_proc -> dialect

---
+++ src/dialect.h
@@ -0,0 +1,8 @@
+#ifndef H_DIALECT
+#define H_DIALECT
+
+#include "object.h"
+
+object make_primitive_procedures_list(void);
+
+#endif
--- src/metacircular-procedures.c
+++ src/metacircular-procedures.c
@@ -1,6 +1,7 @@
 #include "metacircular-procedures.h"
 
 #include "args.h"
+#include "dialect.h"
 #include "list.h"
 #include "primitive-procedures.h"
 #include "sicp-error.h"
@@ -398,36 +399,6 @@ void define_variable(object var, object val, object env)
 // §4.1.4 Running the Evaluator as a Program
 //	https://www.sicp-book.com/book-Z-H-26.html#%_sec_4.1.4
 
-object make_primitive_procedures_list(void)
-{
-	meta_data NMD = NO_META_DATA;
-	return listv( // this comment just keep separate line
-		list2(of_name("+", NMD), of_func(Add, NMD)),
-		list2(of_name("-", NMD), of_func(Sub, NMD)),
-		list2(of_name("*", NMD), of_func(Mul, NMD)),
-		list2(of_name("/", NMD), of_func(Div, NMD)),
-		list2(of_name("abs", NMD), of_func(Abs, NMD)),
-
-		list2(of_name(">", NMD), of_func(Greater_Than, NMD)),
-		list2(of_name("<", NMD), of_func(Less_Than, NMD)),
-		list2(of_name("=", NMD), of_func(Equal, NMD)),
-
-		list2(of_name("and", NMD), of_func(And, NMD)),
-		list2(of_name("or", NMD), of_func(Or, NMD)),
-		list2(of_name("not", NMD), of_func(Not, NMD)),
-
-		list2(of_name("display", NMD), of_func(Display, NMD)),
-		list2(of_name("newline", NMD), of_func(Newline, NMD)),
-		list2(of_name("print", NMD), of_func(Print, NMD)),
-		list2(of_name("print-lines", NMD), of_func(Print_Lines, NMD)),
-		list2(of_name("void", NMD), of_func(Void, NMD)),
-		VA_TERM); // this comment just to keep separate line
-
-	/*
-	 * Potential convience procedures:
-	 * 	square
-	 */
-}
 
 object _primitive_procedures = NULL;
 object primitive_procedures(void)
