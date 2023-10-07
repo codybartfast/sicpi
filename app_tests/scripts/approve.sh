a21c18b8be429c6a24bd583a8ab2e6ec0b521f8f
John Donnellan <john@codybartfast.com>
2023-10-07 16:46:59 +0000

    Mod: use blur for volatile compare

--- app_tests/scripts/approve.sh
+++ app_tests/scripts/approve.sh
@@ -4,10 +4,7 @@
 ###   Check Environment   ###
 #############################
 
-# todo: catch stderr
-
 rpath(){
-	# readlink -f -- "$1"
 	realpath --relative-to="." "$1"
 }
 
@@ -21,13 +18,15 @@ sicp_bin=$(rpath "$base_dir/bin/sicp")
 
 echo "Using '$base_dir' as base directory."
 
-if [[ ! -e "$sicp_bin" ]]; then
+if [[ ! -e "$sicp_bin" ]] ;
+then
 	echo "FATAL: Did not find sicp executable at '$(rpath "$sicp_bin")' ($sicp_bin)."
 	exit 1
 fi
 echo "Found sicp executable: '$(rpath "$sicp_bin")'."
 
-if [[ ! -d "$code_dir" ]]; then
+if [[ ! -d "$code_dir" ]] ;
+then
 	echo "FATAL: Did not find code directory at '$(rpath "$code_dir")' ($code_dir)."
 	exit 1
 fi
@@ -38,31 +37,32 @@ echo "Found code directory: '$(rpath "$code_dir")'."
 #####################
 
 test(){
-	source="$1"
+	local source="$1"
 	echo -n "$source ... "
 
 	local rel_path=$(realpath --relative-to="$code_dir" "$source")
 
-	app_source="$data_dir/$rel_path"
+	local app_source="$data_dir/$rel_path"
 	mkdir -p "$(dirname -- "$app_source")"
 	touch "$app_source"
 
-	expected="$data_dir/$rel_path.approved"
+	local expected="$data_dir/$rel_path.approved"
 	touch "$expected"
 
-	volatile="$data_dir/$rel_path.volatile"
+	local volatile="$data_dir/$rel_path.volatile"
 
-	actual="$data_dir/$rel_path.actual"
+	local actual="$data_dir/$rel_path.actual"
 	"$sicp_bin" < "$source" > "$actual" 2>&1
 
-	if cmp -s "$expected" "$actual" ; then
+	if cmp -s "$expected" "$actual" ;
+	then
 		echo "✓"
-	elif [[ -e $volatile ]] ; then
-		compare_result=$(compare_similar "$expected" "$actual");
-		if [[ $(compare_similar "$expected" "$actual") == true ]] ; then
+	elif [[ -e $volatile ]] ;
+	then
+		if [[ "$(blur "$expected")" == "$(blur "$actual")" ]] ;
+		then
 			echo "~"
 		else
-			echo "FALSE path"
 			review </dev/tty;
 		fi
 	else
@@ -70,28 +70,22 @@ test(){
 	fi
 }
 
-compare_similar(){
-	expected_count=$(cat "$expected" | wc -w )
-	actual_count=$(cat "$actual" | wc -w )
-	if [[ "$expected_count" == "$actual_count" ]] ; then
-		echo true
-	else
-		echo false
-	fi
+blur(){
+	local content=$(cat "$1")
+    	local pattern='[[:digit:]]\+\(\.[[:digit:]]\+\)\?\(e[-+][[:digit:]]\+\)\?'
+	echo "$content" | sed "s/$pattern/<volatile>/g"
 }
 
 review(){
 	echo
 	line "V"
 	echo "Changes in '$source'"
-	# line "."
 	echo
 	diff "$app_source" "$source"
 
 	echo
 	line "."
 	echo "Changes in output"
-	# line "."
 	echo
 	diff "$expected" "$actual"
 
@@ -101,29 +95,32 @@ review(){
 	echo
 
 	declare -A option_names=( ["a"]="Approve" ["v"]="mark as Volatile" )
-	got_response=0;
-	while [[ "$got_response" == 0 ]] ; do
+	local got_response=false;
+	while [[ "$got_response" == false ]] ; do
 		read -p "Approve, Skip or mark Volatile? " -r -n 1 choice
 		echo
-		if [[ "$choice" =~ ^[av]$ ]] ; then
+		if [[ "$choice" =~ ^[av]$ ]] ;
+		then
 			read -p "Definitely want to ${option_names[$choice]}? " -r -n 1 definitely
 			echo
-			if [[ "$definitely" == "d" ]] ; then
+			if [[ "$definitely" == "d" ]] ;
+			then
 				case "$choice" in
 					a)
 				cp "$source" "$app_source"
 				cp "$actual" "$expected"
-				got_response=1
+				got_response=true
 					;;
 					v)
 				touch "$volatile"
-				got_response=1
+				got_response=true
 					;;
 				esac
 			fi
-		elif [[ "$choice" == "s" ]] ; then
+		elif [[ "$choice" == "s" ]] ;
+		then
 			echo "Skipping"
-			got_response=1
+			got_response=true
 		fi
 	done
 
@@ -144,13 +141,15 @@ line(){
 code_files=$(find "$code_dir" -type f -name "*.sicp" | sort)
 code_file_count=$(echo "$code_files" | wc -l)
 
-if [[  0 -eq "$code_file_count" ]]; then
+if [[  0 -eq "$code_file_count" ]] ;
+then
 	echo "EXITING: no code files found."
 	exit 1
 fi
 echo "Found $code_file_count code files."
 
-echo "$code_files" | while read -r line ; do
-   test "$line"
+echo "$code_files" | while read -r code_file ;
+do
+   test "$code_file"
 done
 echo "Done"
